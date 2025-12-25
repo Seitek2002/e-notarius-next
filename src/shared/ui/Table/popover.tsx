93c79fe1f913008b9f448f42c740c8468e540add
Seitek2002 <seitekbegaliev@gmail.com>
2025-12-25 22:09:01 +0600

    popover template add

--- src/shared/ui/Table/popover.tsx
+++ src/shared/ui/Table/popover.tsx
@@ -1,3 +1,46 @@
-export const Popover = () => {
-  return <div>Popover</div>;
+import { FC, useEffect, useRef } from 'react';
+import clsx from 'clsx';
+
+import { Checkbox } from '../Input';
+
+import './style.css';
+
+type TPopover = {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+};
+
+export const Popover: FC<TPopover> = ({ isOpen, setIsOpen }) => {
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (
+        popoverRef.current &&
+        !popoverRef.current.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className={clsx('table-popover', { open: isOpen })} ref={popoverRef}>
+      <div className='table-popover__content'>
+        <Checkbox label='Доверенность' name='Доверенность' />
+        <Checkbox label='Договор' name='Договор' />
+        <Checkbox label='Соглашение' name='Соглашение' />
+        <Checkbox label='Завещание' name='Завещание' />
+        <Checkbox label='Иск' name='Иск' />
+        <Checkbox label='Хадатойство' name='Хадатойство' />
+      </div>
+      <button>Готово</button>
+    </div>
+  );
 };
--- src/shared/ui/Table/table-header-cell.tsx
+++ src/shared/ui/Table/table-header-cell.tsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import clsx from 'clsx';
+
+import { Popover } from './popover';
+
 import { Application, Column } from '@/widgets/table/applications/model';
+
+import filterIcon from '@assets/icons/table/filter-icon.svg';
+import sortIcon from '@assets/icons/table/sort-icon.svg';
 
 export const TableHeaderCell = ({
   column,
 }: {
   column: Column<Application>;
 }) => {
+  const [showPopover, setShowPopover] = useState(false);
+  const [sortActive, setSortActive] = useState(false);
+
+  const handleClick = () => {
+    if (column.filterable) {
+      setShowPopover(true);
+    } else if (column.sortable) {
+      setSortActive(!sortActive);
+    } else {
+      return null;
+    }
+  };
+
   return (
     <th
       style={{
@@ -13,7 +35,27 @@ export const TableHeaderCell = ({
         textAlign: column.align ?? 'left',
       }}
     >
-      {column.label}
+      <div
+        onClick={handleClick}
+        className={clsx('table-header-cell', {
+          clickable: column.sortable || column.filterable,
+        })}
+      >
+        <span>{column.label}</span>
+
+        {column.sortable && (
+          <Image
+            src={sortIcon}
+            alt='sortIcon'
+            style={{ transform: `rotate(${sortActive ? 180 : 0}deg)` }}
+          />
+        )}
+        {column.filterable && <Image src={filterIcon} alt='filterIcon' />}
+
+        {column.filterable && (
+          <Popover isOpen={showPopover} setIsOpen={setShowPopover} />
+        )}
+      </div>
     </th>
   );
 };

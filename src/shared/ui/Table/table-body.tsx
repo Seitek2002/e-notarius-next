e83b30d2afbd7b03e2422840576618211f21a38c
Seitek2002 <seitekbegaliev@gmail.com>
2025-12-28 22:59:15 +0600

    add row actions

--- src/shared/ui/Table/table-body.tsx
+++ src/shared/ui/Table/table-body.tsx
@@ -2,6 +2,11 @@ import { FC } from 'react';
 import { TableCell } from './table-cell';
 import { Application, Column } from '@/widgets/table/applications/model';
 
+import DetailsIcon from '@assets/icons/table/details-icon.svg';
+import EditIcon from '@assets/icons/table/edit-icon.svg';
+import DownloadIcon from '@assets/icons/table/download-icon.svg';
+import DeleteIcon from '@assets/icons/table/delete-icon.svg';
+
 type TProps = {
   columns: Column<Application>[];
   data: Application[];
@@ -15,6 +20,12 @@ export const TableBody: FC<TProps> = ({ columns, data }) => {
           {columns.map((column) => (
             <TableCell key={column.key} column={column} row={row} />
           ))}
+          <td className='icons'>
+            <DetailsIcon />
+            <EditIcon />
+            <DownloadIcon />
+            <DeleteIcon />
+          </td>
         </tr>
       ))}
     </tbody>

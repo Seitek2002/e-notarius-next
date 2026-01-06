326e1f517cc9d3d16d2cf53a7beb0994be95ec41
Seitek2002 <seitekbegaliev@gmail.com>
2026-01-06 17:12:05 +0600

    header title dynamic

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/widgets/header/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex w-full'>
+    <div className='flex w-full min-h-screen'>
       <Sidebar />
       <div className='flex-1 min-w-0 px-5 lg:px-14'>
         <Header />
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,11 +1,23 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+import { navs, publicNavs } from '../sidebar/model/navigation';
+
 import DefaultAvatar from '@assets/icons/header/default-avatar.svg';
 import ArrowIcon from '@assets/icons/header/arrow.svg';
 import NotificationIcon from '@assets/icons/header/notification.svg';
 
 const Header = () => {
+  const pathname = usePathname();
+
+  const pageTitle =
+    [...navs, ...publicNavs].find((n) => n.path === pathname)?.label ||
+    'Заявки';
+
   return (
     <header className='flex items-center justify-between py-6'>
-      <h2 className='text-main-green font-semibold text-[24px]'>Заявки</h2>
+      <h2 className='text-main-green font-semibold text-[24px]'>{pageTitle}</h2>
       <div className='flex items-center gap-[30px] text-[16px] text-dark-blue'>
         <div className='flex items-center gap-2 font-medium cursor-pointer'>
           <DefaultAvatar />
